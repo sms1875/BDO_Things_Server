51e99e2e9fd92afb4da2e68d186bebe04135e058
sms1875 <01audtjr@gmail.com>
2024-02-14 16:21:14 +0900

    Feat : 등록 대기 상품 구현
    APi를 통한 거래소 등록 대기 상품 웹페이지 표시 구현

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const MarketApi = require('./MarketApi');
 
 const app = express();
 
@@ -9,6 +10,21 @@ app.get('/', function (req, res) {
 
 app.get('/main', function (req, res) {
     res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get('/marketWaitList', async function (req, res) {
+    try {
+        const marketWaitList = await MarketApi.getWorldMarketWaitList();
+        
+        if (marketWaitList) {
+            res.status(200).json(marketWaitList);
+        } else {
+            res.status(500).json({ error: 'Error fetching market wait list' });
+        }
+    } catch (error) {
+        console.error('Error fetching market wait list:', error);
+        res.status(500).json({ error: 'Error fetching market wait list' });
+    }
 });
 
 app.listen(3000, function () {
