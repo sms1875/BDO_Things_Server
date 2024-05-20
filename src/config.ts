4b5666401ccfcb79aa23886fed2a1c1095098fb9
sms1875 <01audtjr@gmail.com>
2024-05-20 13:24:50 +0900

    refactor: config 구조 변경
    config, firebaseConfig 인터페이스 생성

--- src/config.ts
+++ src/config.ts
@@ -2,29 +2,33 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const config: {
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId?: string;
+}
+
+interface Config {
     port: string | undefined;
-    firebaseConfig: {
-        apiKey: string;
-        authDomain: string;
-        projectId: string;
-        storageBucket: string;
-        messagingSenderId: string;
-        appId: string;
-        measurementId?: string;
-    };
-} = {
+    firebaseConfig: FirebaseConfig;
+}
+
+const config: Config = {
     port: process.env.PORT,
     firebaseConfig: {
-        apiKey: process.env.FIREBASE_API_KEY || "YOUR_API_KEY",
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-        projectId: process.env.FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
+        apiKey: process.env.FIREBASE_API_KEY ?? "YOUR_API_KEY",
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN ?? "YOUR_AUTH_DOMAIN",
+        projectId: process.env.FIREBASE_PROJECT_ID ?? "YOUR_PROJECT_ID",
         storageBucket:
-            process.env.FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
+            process.env.FIREBASE_STORAGE_BUCKET ?? "YOUR_STORAGE_BUCKET",
         messagingSenderId:
-            process.env.FIREBASE_MESSAGING_SENDER_ID ||
+            process.env.FIREBASE_MESSAGING_SENDER_ID ??
             "YOUR_MESSAGING_SENDER_ID",
-        appId: process.env.FIREBASE_APP_ID || "YOUR_APP_ID",
+        appId: process.env.FIREBASE_APP_ID ?? "YOUR_APP_ID",
         measurementId:
             process.env.FIREBASE_MEASUREMENT_ID || "YOUR_MEASUREMENT_ID"
     }
