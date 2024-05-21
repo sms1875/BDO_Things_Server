689ecf9744e8541517a1cec81a80037a77739542
sms1875 <01audtjr@gmail.com>
2024-05-21 17:42:59 +0900

    test:cors 설정 변경
    cors 통신 테스트

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: "*", // 클라이언트의 도메인으로 변경
+    origin: "https://sms1875.github.io/BDO-Things", // 클라이언트의 도메인으로 변경
     methods: "GET,POST", // 허용할 HTTP 메서드
     allowedHeaders: "Content-Type,Authorization" // 허용할 HTTP 헤더
 };
